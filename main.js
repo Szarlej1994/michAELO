b852c11093952504861a5302d5152497f56db24f
Michał Szarek <115929927+Szarlej1994@users.noreply.github.com>
2023-03-09 15:31:26 +0100

    Add files via upload
    Strona pokazująca działanie winiarni

---
+++ main.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('.navbar-collapse')
+
+document.addEventListener('click', () => {
+  if (nav.classList.contains('show')) {
+    nav.classList.remove('show')
+  }
+})
+
